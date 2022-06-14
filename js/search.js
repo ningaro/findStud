fdd6db6007cf5080c9129a4cc8e54cd6495ba4f5
Polina <78511103+PolinaVvv@users.noreply.github.com>
2022-06-14 15:58:35 +0300

    v1

--- js/search.js
+++ js/search.js
@@ -15,79 +15,38 @@ document.getElementById('close-alert').addEventListener('click', () => {
   document.cookie = 'alert=allreadyread'
 })
 
-// тут
-document.getElementById('form').onsubmit = function (evt) {
-  {
-    document.getElementById(
-      'jumbotron'
-    ).innerHTML = `<div class="d-flex" style=""><div class="spinner-grow mx-auto" role="status"><span class="sr-only">Загрузка...</span>`
+document.getElementById('form').onsubmit = (evt) => {
+  evt.preventDefault()
+  document.getElementById(
+    'jumbotron'
+  ).innerHTML = `<div class="d-flex" style=""><div class="spinner-grow mx-auto" role="status"><span class="sr-only">Загрузка...</span>`
 
-    let searchStr = document.getElementById('search-field').value
-    searchStr = searchStr.toLowerCase()
-    searchStr = searchStr.split(' ')
-    searchStr.sort()
+  let searchStr = document.getElementById('search-field').value
+  searchStr = searchStr.toLowerCase()
+  searchStr = searchStr.split(' ')
+  searchStr.sort()
 
-    let readyList = ''
-    for (let i = 0; i < data.studDocs.length; i++) {
-      let nStr = data.studDocs[i].name.toLowerCase()
-      nStr = nStr.split(' ')
-      nStr.sort()
-      for (let j = 0; j < searchStr.length; j++) {
-        if (nStr.includes(searchStr[j]) && j == searchStr.length - 1) {
-          readyList =
-            readyList +
-            `<h5 class="">${data.studDocs[i].studNum} <small class="text-muted">${data.studDocs[i].name}</small></h5>`
-        } else if (nStr.includes(searchStr[j])) {
-          continue
-        } else {
-          break
-        }
+  let readyList = ''
+  for (let i = 0; i < data.studDocs.length; i++) {
+    let nStr = data.studDocs[i].name.toLowerCase()
+    nStr = nStr.split(' ')
+    nStr.sort()
+    for (let j = 0; j < searchStr.length; j++) {
+      if (nStr.includes(searchStr[j]) && j == searchStr.length - 1) {
+        readyList =
+          readyList +
+          `<h5 class="">${data.studDocs[i].studNum} <small class="text-muted">${data.studDocs[i].name}</small></h5>`
+      } else if (nStr.includes(searchStr[j])) {
+        continue
+      } else {
+        break
       }
-    }
-
-    if (readyList != '') {
-      document.getElementById('jumbotron').innerHTML = readyList
-    } else {
-      document.getElementById('jumbotron').innerHTML = `😥 Ничего не найдено.`
     }
   }
-  return false
+
+  if (readyList != '') {
+    document.getElementById('jumbotron').innerHTML = readyList
+  } else {
+    document.getElementById('jumbotron').innerHTML = `😥 Ничего не найдено.`
+  }
 }
-// тут
-
-document
-  .getElementById('button-search')
-  .addEventListener('click', function (evt) {
-    document.getElementById(
-      'jumbotron'
-    ).innerHTML = `<div class="d-flex" style=""><div class="spinner-grow mx-auto" role="status"><span class="sr-only">Загрузка...</span>`
-
-    let searchStr = document.getElementById('search-field').value
-    searchStr = searchStr.toLowerCase()
-    searchStr = searchStr.split(' ')
-    searchStr.sort()
-
-    let readyList = ''
-    for (let i = 0; i < data.studDocs.length; i++) {
-      let nStr = data.studDocs[i].name.toLowerCase()
-      nStr = nStr.split(' ')
-      nStr.sort()
-      for (let j = 0; j < searchStr.length; j++) {
-        if (nStr.includes(searchStr[j]) && j == searchStr.length - 1) {
-          readyList =
-            readyList +
-            `<h5 class="">${data.studDocs[i].studNum} <small class="text-muted">${data.studDocs[i].name}</small></h5>`
-        } else if (nStr.includes(searchStr[j])) {
-          continue
-        } else {
-          break
-        }
-      }
-    }
-
-    if (readyList != '') {
-      document.getElementById('jumbotron').innerHTML = readyList
-    } else {
-      document.getElementById('jumbotron').innerHTML = `😥 Ничего не найдено.`
-    }
-  })
